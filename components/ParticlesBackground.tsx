31164fc6d85742ea92391d6fd3aaf0a0c3532bc7
Quanosek <kubaklalo@gmail.com>
2023-01-21 19:05:19 +0100

    init

---
+++ components/ParticlesBackground.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+
+import { useCallback } from "react";
+import type { Engine } from "tsparticles-engine";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+
+import styles from "@/styles/home.module.scss";
+
+export default function ParticlesBackground() {
+  const particlesInit = useCallback(async (engine: Engine) => {
+    await loadFull(engine);
+  }, []);
+
+  return (
+    <Particles
+      id="tsparticles"
+      className={styles.particles}
+      init={particlesInit}
+      options={{
+        fpsLimit: 90,
+        fullScreen: {
+          enable: false,
+          zIndex: 0,
+        },
+        interactivity: {
+          events: {
+            onHover: {
+              enable: true,
+              mode: "bubble",
+            },
+          },
+          modes: {
+            bubble: {
+              opacity: 1,
+              size: 3.5,
+              color: {
+                value: "#ccd5f9",
+              },
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#6e86ef",
+          },
+          links: {
+            color: {
+              value: "#6e86ef",
+            },
+            enable: true,
+            opacity: 1,
+          },
+          move: {
+            enable: true,
+          },
+          opacity: {
+            value: 0.8,
+          },
+          number: {
+            value: 120,
+          },
+          size: {
+            value: 2,
+          },
+        },
+        setResponsive: {
+          pxRatio: 1,
+        },
+      }}
+    />
+  );
+}
