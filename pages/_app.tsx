bb96eb0c315b769b69de584a8402c29b43b22929
Quanosek <kubaklalo@gmail.com>
2023-03-03 10:49:17 +0100

    google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,24 @@ import { useEffect } from "react";
 
 import { ParallaxProvider } from "react-scroll-parallax";
 
+import type { NextWebVitalsMetric } from "next/app";
+import { GoogleAnalytics, event } from "nextjs-google-analytics";
 import { GoogleAdSense } from "nextjs-google-adsense";
-import { GoogleAnalytics } from "nextjs-google-analytics";
 
 import "the-new-css-reset/css/reset.css";
 import "@/styles/globals.scss";
+
+export function reportWebVitals(metric: NextWebVitalsMetric) {
+  event(metric.name, {
+    category:
+      metric.label === "web-vital" ? "Web Vitals" : "Next.js custom metric",
+    value: Math.round(
+      metric.name === "CLS" ? metric.value * 1000 : metric.value
+    ),
+    label: metric.id,
+    nonInteraction: true,
+  });
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   // hiding loader screen
@@ -31,8 +44,13 @@ export default function App({ Component, pageProps }: AppProps) {
       </Head>
 
       <ParallaxProvider>
-        <GoogleAdSense publisherId={"env.local"} />
-        <GoogleAnalytics trackPageViews />
+        <GoogleAnalytics
+          gaMeasurementId={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID as string}
+          trackPageViews
+        />
+        <GoogleAdSense
+          publisherId={process.env.NEXT_PUBLIC_ADSENSE_PUBLISHER_ID as string}
+        />
 
         <Component {...pageProps} />
       </ParallaxProvider>
